f1b08f69e5542ffb0b0c7005031da5227f2931d5
Heather Hudgeon <hhudgeon@gmail.com>
2026-02-02 07:37:31 -0600

    Fixed mobile sidenav behavior and updated jQuery version

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 <!---------------- END footer ------------------>
 
 <!--  Scripts-->
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="js/materialize.js"></script>
 <script src="js/init.js"></script>
 
--- js/init.js
+++ js/init.js
@@ -4,5 +4,15 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    // Close sidenav when resizing to desktop width
+    $(window).on('resize', function () {
+      if ($(window).width() >= 992) {
+        var instance = M.Sidenav.getInstance(document.querySelector('.sidenav'));
+        if (instance && instance.isOpen) {
+          instance.close();
+        }
+      }
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
